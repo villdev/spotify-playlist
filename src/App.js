b4d1755ee032e55f4625fa3d49d5c536b73112d8
vill <villdevelop@gmail.com>
2020-12-20 15:26:46 +0530

    chore: added initial boilerplate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./css/style.css";
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <h1>
+        {" "}
+        <span role="img" aria-label="music icon">
+          🎵
+        </span>{" "}
+        Spotify Playlist
+      </h1>
+    </div>
+  );
+}
